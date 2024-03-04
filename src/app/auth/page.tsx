f1b1a6a0403c5e948cb69dff4c83f7532c2bf66f
Abhishek Dhar <dhar.abhishek999@gmail.com>
2024-03-04 23:28:11 +0530

    update

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -60,8 +60,11 @@ export default function Auth() {
                 <Link href={"/MakeNotes"}>
                             <button type="button" className= "  text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><span className="flex"><span>Make Notes</span> <span className="my-0.5 mx-1"><BiNotepad className="text-lg" /></span></span></button>
                             </Link>
+                            <Link href={"/HaveFun"}>
                             <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><span className="flex"><span>Have Fun</span> <span className="my-0.5 mx-1"><IoGameControllerOutline className="text-lg" />
+
 </span></span></button>
+</Link>
                         </div>
                 </div>
                 <div className="links flex  ">
